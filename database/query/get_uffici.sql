7d4a103c6bf37eec09232fa85de0a0a158ed7c5c
GiulioDFL <lunafastpk+github@gmail.com>
2026-01-18 02:16:45 +0100

    formato

--- database/query/get_uffici.sql
+++ database/query/get_uffici.sql
@@ -5,12 +5,21 @@ SELECT
     u.cva_tipo_ufficio_id, 
     u.nome_ufficio,
     cva.valore as tipo_ufficio,
+    soc.ragione_sociale,
+    i.via as sede_via,
+    i.numero_civico as sede_numero_civico,
+    i.cap as sede_cap,
+    i.comune as sede_comune,
+    i.provincia as sede_provincia,
+    i.paese as sede_paese,
+    cva_sede.valore as sede_tipo,
     CASE 
         WHEN u.sede_id IS NOT NULL THEN 'Sede ' || cva_sede.valore || ' - ' || i.comune || ' (' || i.provincia || ')'
         ELSE 'Sede Legale / Intera Società' 
     END as collocazione
 FROM uffici u
 JOIN chiave_valore_attributo cva ON u.cva_tipo_ufficio_id = cva.id
+JOIN societa soc ON u.societa_id = soc.id
 LEFT JOIN sedi s ON u.sede_id = s.id
 LEFT JOIN chiave_valore_attributo cva_sede ON s.cva_tipo_sede_id = cva_sede.id
 LEFT JOIN indirizzi i ON s.indirizzo_id = i.id
